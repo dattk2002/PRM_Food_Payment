feb4bce43130ae3d66b937316cd6ef4e2f5b766c
tiep-em <tiepkhodo@gmail.com>
2023-07-07 21:58:49 +0700

    add Login/Register

--- app/src/main/java/com/example/prm_assignment_foodanddrink/SignUpActivity.java
+++ app/src/main/java/com/example/prm_assignment_foodanddrink/SignUpActivity.java
@@ -1,14 +1,67 @@
 package com.example.prm_assignment_foodanddrink;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class SignUpActivity extends AppCompatActivity {
+    private EditText etUsername, etPassword, etConfirmPassword;
+    private Button btnSignUp;
+
+    private TextView tvSignIn;
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        etUsername = findViewById(R.id.editTextUsername);
+        etPassword = findViewById(R.id.editTextPassword);
+        btnSignUp = findViewById(R.id.buttonSignUp);
+        etConfirmPassword = findViewById(R.id.editTextConfirm);
+        tvSignIn = findViewById(R.id.textViewAlreadyAccount);
+        databaseHelper = new DatabaseHelper(this);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                String confirmPassword = etConfirmPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(SignUpActivity.this, "Please enter all fields", Toast.LENGTH_SHORT).show();
+                } else if (!password.equals(confirmPassword)) {
+                    Toast.makeText(SignUpActivity.this, "Password and Confirm Password do not match", Toast.LENGTH_SHORT).show();
+                } else {
+                    boolean success = databaseHelper.addUser(username, password);
+                    if (success) {
+                        Toast.makeText(SignUpActivity.this, "Account has activated", Toast.LENGTH_SHORT).show();
+                        // Chuyển hướng đến màn hình đăng nhập
+                        startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
+                        finish();
+                    } else {
+                        Toast.makeText(SignUpActivity.this, "Username had existed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        tvSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
